5c6b3a95069f884d9bcd761802dd62d2156b0dbd
shakawat20 <shakawat.cse20@gmail.com>
2023-09-08 19:58:03 +0600

    hello

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,18 +2,18 @@
 
 
 import React, { useState, useEffect } from 'react';
- import { useAuthState } from 'react-firebase-hooks/auth';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '../../firebase/Firebase';
 import Navbar from '../navbar/Navbar';
 const Home = () => {
   // State to store task data
   const [tasks, setTasks] = useState([]);
-  const [user,loading,error]=useAuthState(auth)
- 
-  
+  const [user, loading, error] = useAuthState(auth)
+
+
 
   // Simulated data for demonstration purposes
-  const sampleTasks =   JSON.parse(localStorage.getItem('tasks')) || [];
+  const sampleTasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
   useEffect(() => {
     // In a real application, you would fetch task data from your backend or storage.
@@ -28,38 +28,61 @@ const Home = () => {
 
   return (
     <div>
-     
-       <div className="min-h-screen bg-gray-100 p-4">
-      <h2 className="text-2xl font-semibold mb-4">Dashboard</h2>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        <div className="bg-white p-4 rounded shadow-md">
-          <h3 className="text-xl font-semibold mb-2">Completed Tasks</h3>
-          <p className="text-3xl font-bold">{completedTasks}</p>
+
+      <div className="min-h-screen bg-gray-100 p-4">
+        <h2 className="text-2xl font-semibold mb-4">Dashboard</h2>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+          <div className="bg-white p-4 rounded shadow-md">
+            <h3 className="text-xl font-semibold mb-2">Completed Tasks</h3>
+            <p className="text-3xl font-bold">{completedTasks}</p>
+          </div>
+          <div className="bg-white p-4 rounded shadow-md">
+            <h3 className="text-xl font-semibold mb-2">In Progress Tasks</h3>
+            <p className="text-3xl font-bold">{inProgressTasks}</p>
+          </div>
+          <div className="bg-white p-4 rounded shadow-md">
+            <h3 className="text-xl font-semibold mb-2">Pending Tasks</h3>
+            <p className="text-3xl font-bold">{pendingTasks}</p>
+          </div>
         </div>
-        <div className="bg-white p-4 rounded shadow-md">
-          <h3 className="text-xl font-semibold mb-2">In Progress Tasks</h3>
-          <p className="text-3xl font-bold">{inProgressTasks}</p>
-        </div>
-        <div className="bg-white p-4 rounded shadow-md">
-          <h3 className="text-xl font-semibold mb-2">Pending Tasks</h3>
-          <p className="text-3xl font-bold">{pendingTasks}</p>
-        </div>
+        <table className="min-w-full divide-y divide-gray-200">
+          <thead>
+            <tr>
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Title
+              </th>
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Status
+              </th>
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                priority
+              </th>
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Due date
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {tasks.map((task) => (
+              <tr key={task.id}>
+                <td className="px-6 py-4 whitespace-nowrap text-lg">{task.title}</td>
+                <td className={`px-6 py-4 whitespace-nowrap text-${task.status === 'completed' ? 'green' : 'blue'}-500`}>
+                  {task.status}
+                </td>
+                <td className={`px-6 py-4 whitespace-nowrap text-${task.status === 'completed' ? 'green' : 'blue'}-500`}>
+                  {task.priority}
+                </td>
+                <td className={`px-6 py-4 whitespace-nowrap text-${task.status === 'completed' ? 'green' : 'blue'}-500`}>
+                  {task.dueDate}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
       </div>
-      <h3 className="text-xl font-semibold mt-8">Upcoming Tasks</h3>
-      <ul className="list-disc list-inside">
-        {/* Display a list of upcoming tasks here */}
-        {tasks.map((task) => (
-          <li key={task.id} className="mt-2">
-            <span className="text-lg">{task.title}</span>
-            <span className={`ml-2 text-${task.status === 'completed' ? 'green' : 'blue'}-500`}>
-              Status: {task.status}
-            </span>
-          </li>
-        ))}
-      </ul>
     </div>
-    </div>
-   
+
   );
 };
 
